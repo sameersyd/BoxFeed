773931c9b6e3833d11e0ca78ab45067b745572c1
Sameer Nawaz <sameer.nwaz@gmail.com>
2021-07-06 00:11:17 +0530

    Delete default persistence

---
+++ BoxFeed/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  BoxFeed
+//
+//  Created by Sameer Nawaz on 06/07/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
