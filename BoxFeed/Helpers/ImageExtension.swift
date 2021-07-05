d766d8ac340b701eba7859fb467f30d0171fe54c
Sameer Nawaz <sameer.nwaz@gmail.com>
2021-07-06 01:02:29 +0530

    Add ImageExtension

---
+++ BoxFeed/Helpers/ImageExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ImageExtension.swift
+//  BoxFeed
+//
+//  Created by Sameer Nawaz on 06/07/21.
+//
+
+import SwiftUI
+
+extension Image {
+    
+    // Icons
+    static let back_arrow = Image("back-arrow")
+    static let clock = Image("clock")
+    static let calender = Image("calender")
+    static let bookmark = Image("bookmark")
+    static let bookmark_filled = Image("bookmark-filled")
+}
